a0e0c7f6b8615ac89e05652e1b392b3433424f4e
chinweugwu <ugwu.millicent@gmail.com>
2023-02-23 06:16:45 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print all the single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
