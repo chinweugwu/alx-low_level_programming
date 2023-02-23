00bd7bd12901d01cad7d83c6aaad02f2e59012fc
chinweugwu <ugwu.millicent@gmail.com>
2023-02-23 04:30:12 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/** main - A prgram that prints the alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	char  alps[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for(i = 0; i < 26; i++)
+	 {
+	 	putchar(alp[i]);
+	 }
+	putchar('\n');
+	return(0);
+}	
