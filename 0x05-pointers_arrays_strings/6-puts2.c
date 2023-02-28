9f19533dda9a159b631bcb0d1c82748285104008
chinweugwu <ugwu.millicent@gmail.com>
2023-02-28 07:02:29 -0800

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int word = 0;
+	int t = 0;
+	char *y = str;
+	int les;
+
+	while (*y != '\0')
+	{
+		y++;
+		word++;
+	}
+	t = longi - 1;
+	for (les = 0 ; les <= t ; les++)
+	{
+		if (les % 2 == 0)
+	{
+		_putchar(str[les]);
+	}
+	}
+	_putchar('\n');
+}
