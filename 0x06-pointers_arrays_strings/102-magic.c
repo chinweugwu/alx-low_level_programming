683aa1b405cd0bbc4c56e1f7dcc8d1bb74522ef4
chinweugwu <ugwu.millicent@gmail.com>
2023-03-02 12:50:38 -0800

    Correction

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
-/**
- * main - correct a piece of code
- * Return: 0
- */
 
 int main(void)
 {
-	 int n;
-	 int a[5];
-	 int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	 a[2] = 1024;
-	 p = &n;
+  a[2] = 1024;
+  p = &n;
 
-	 *(p = &a[2] - 1) = 98;
-	 printf("a[2] = %d\n", a[2]);
-	return (0);
+  *(p = &a[2] - 1) = 98;
+
+  printf("a[2] = %d\n", a[2]);
+  return 0;
 }
