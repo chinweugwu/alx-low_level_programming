bcfa87d2d5608986fed7b82c7de31ab4b7776d8b
chinweugwu <ugwu.millicent@gmail.com>
2023-03-23 16:25:29 -0700

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns prime number confirmation value, either 0 or 1
+ * @n : accused prime number suspect
+ * Return: 0 if innocent, 1 if guilty of being a prime number
+ */
+
+int is_prime_number(int n)
+{
+	int i;
+
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+		return (0);
+		}
+		else
+		{
+		return (1);
+		}
+	}
+}
