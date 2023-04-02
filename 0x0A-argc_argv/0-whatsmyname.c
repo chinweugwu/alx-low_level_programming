72e2830cbf2b6771899870518aae8d018c9797e1
chinweugwu <ugwu.millicent@gmail.com>
2023-04-02 15:01:37 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - to list name of file using argc
+ * @argc: input argument total number
+ * @argv: array of input arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s/n", argv[0]);
+	return (0);
+}
