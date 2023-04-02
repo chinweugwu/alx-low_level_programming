885c19b2d66db1dc057a6fd95d292f327f33194c
chinweugwu <ugwu.millicent@gmail.com>
2023-04-02 16:10:23 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/*
+ * main - prints the number of arguments
+ * @argc: number of inputs
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
