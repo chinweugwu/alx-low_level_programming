28f9280907fa245c136518e64772194c9a2d3f05
chinweugwu <ugwu.millicent@gmail.com>
2023-04-02 16:32:07 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,14 +3,15 @@
 #include "main.h"
 
 /*
- * main - prints the number of arguments
+ * main - prints the number of arguments passed to the program
  * @argc: number of inputs
+ * @argv: array of arguments
  * Return: 0
  */
 
-int main(int argc)
+int main(int argc, char *argv[])
 {
+	(void) argv; /*ignore argv*/
 	printf("%d\n", argc - 1);
-
 	return (0);
 }
