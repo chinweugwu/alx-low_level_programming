e51ea9c39281ebae2a0b00d96252fcb61b02c1b3
chinweugwu <ugwu.millicent@gmail.com>
2023-04-01 21:01:04 -0700

    Task 0 correction

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
@@ -6,9 +8,9 @@
 
 void _puts(char *str)
 {
-        while (*str != '\0')
-        {
-                _putchar(*str++);
-        }
-                _putchar('\n');
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
 }
